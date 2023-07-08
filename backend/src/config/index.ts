31323427dbe207596ca1f9fb561e919ab80b341a
Pedro Weber Mendonça <pedro.weber@banqi.com>
2023-07-08 19:13:55 -0300

    👷 ✅ Added end to end tests

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,6 +2,7 @@ const config = {
   activeEnv: process.env.NODE_ENV || 'dev',
   serviceName: 'grocery-list-backend',
   exampleVariable: process.env.EXAMPLE_ENV,
+  awsRegion: process.env.AWS_REGION || 'us-east-1',
 };
 
 export { config };
--- backend/tests/e2e/features/test-feature/handler.spec.ts
+++ backend/tests/e2e/features/test-feature/handler.spec.ts
@@ -1,22 +1,53 @@
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
-import { stub } from 'sinon';
+import { CloudFormation, SharedIniFileCredentials } from 'aws-sdk';
+import axios, { AxiosInstance } from 'axios';
 
-import { handler } from '@features/test-feature';
+import { describe, it } from 'mocha';
+import { expect, use } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import { config } from '@src/config';
+
+use(chaiAsPromised);
+
+const credentials = new SharedIniFileCredentials({ profile: 'grocery-list-testing' });
+
+const cloudformation = new CloudFormation({
+  credentials,
+  region: config.awsRegion,
+});
+
+const getApiURL = async (stackName: string) => {
+  try {
+    const resources = await cloudformation.describeStackResources({ StackName: stackName }).promise();
+
+    const apiResource = resources.StackResources?.find(resource => resource.LogicalResourceId === 'ApiGatewayRestApi');
+
+    if (!apiResource) {
+      throw new Error('Could not find API resource');
+    }
+
+    const apiId = apiResource.PhysicalResourceId;
+
+    return `https://${apiId}.execute-api.${config.awsRegion}.amazonaws.com`;
+  } catch (error) {
+    console.log('error while retrieving api url', error);
+
+    throw error;
+  }
+};
 
 describe('Example end to end test', () => {
-  const testFunction = stub();
-  console.log('Running integration test for stage', process.env.STAGE_NAME);
+  const stageName = process.env.STAGE_NAME;
+  let httpClient: AxiosInstance;
 
-  it('function should be called', () => {
-    testFunction();
-
-    expect(testFunction.callCount).to.be.equal(1);
+  before(async () => {
+    const apiURL = await getApiURL(`${config.serviceName}-${stageName}`);
+    httpClient = axios.create({ baseURL: apiURL, validateStatus: () => true });
   });
 
-  it('handler should return statusCode 200', async () => {
-    const response = await handler();
+  it('endpoint should return statusCode 200', async () => {
+    const response = await httpClient.get(`${stageName}/test`);
 
-    expect(response.statusCode).to.be.equal(200);
+    expect(response.status).to.be.equal(200);
   });
 });
