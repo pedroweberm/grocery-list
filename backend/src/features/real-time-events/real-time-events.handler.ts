d655f794859ac1b8cb3ce9d18e87f2caeded1a0c
Pedro Weber Mendonça <pedro.weber@banqi.com>
2023-07-12 01:35:58 -0300

    🐛 Fixed presigned url generation

--- backend/src/features/real-time-events/real-time-events.handler.ts
+++ backend/src/features/real-time-events/real-time-events.handler.ts
@@ -1,24 +1,14 @@
 import type { APIGatewayEvent } from 'aws-lambda';
 import { STSClient, AssumeRoleCommand } from '@aws-sdk/client-sts';
-import crypto from 'crypto';
 import { v4 as uuid } from 'uuid';
+import { SignatureV4 } from '@aws-sdk/signature-v4';
+import { Sha256 } from '@aws-crypto/sha256-js';
+import { HttpRequest } from '@aws-sdk/protocol-http';
+import { formatUrl } from '@aws-sdk/util-format-url';
 
 import { config } from '@src/config';
 
-function sign(key: Buffer, msg: string) {
-  return crypto.createHmac('sha256', key).update(msg, 'utf-8').digest();
-}
-
-function getSignatureKey(key: string, dateStamp: string, regionName: string, serviceName: string) {
-  const kDate = sign(Buffer.from('AWS4' + key, 'utf-8'), dateStamp);
-  const kRegion = sign(kDate, regionName);
-  const kService = sign(kRegion, serviceName);
-  const kSigning = sign(kService, 'aws4_request');
-
-  return kSigning;
-}
-
-const createPresignedURL = ({
+const createPresignedURL = async ({
   accessKeyId,
   secretAccessKey,
   sessionToken,
@@ -41,42 +31,26 @@ const createPresignedURL = ({
   service: string;
   expirationTime: number;
 }) => {
-  // Get the current date and time
-  const currentDate = new Date(new Date().toUTCString());
-  const dateTime = currentDate.toISOString().replace(/[:-]/g, '').slice(0, -5) + 'Z';
-  const date = currentDate.toISOString().slice(0, 10);
+  const signer = new SignatureV4({
+    credentials: { accessKeyId, secretAccessKey, sessionToken: undefined },
+    region,
+    service,
+    sha256: Sha256,
+  });
+  const req = new HttpRequest({
+    protocol,
+    hostname: host,
+    path,
+    method,
+    headers: { host },
+  });
+  const signedRequest = await signer.presign(req, { expiresIn: expirationTime });
 
-  // Define the signing algorithm
-  const algorithm = 'AWS4-HMAC-SHA256';
+  if (typeof sessionToken === 'string' && typeof signedRequest.query === 'object') {
+    signedRequest.query['X-Amz-Security-Token'] = sessionToken;
+  }
 
-  // Define the credential scope
-  const credentialScope = `${date}/${region}/${service}/aws4_request`;
-
-  // Build the canonical query string
-  const canonicalQuerystring = `X-Amz-Algorithm=${algorithm}$X-Amz-Credential=${encodeURIComponent(
-    `${accessKeyId}/${credentialScope}`,
-  )}&X-Amz-Date=${dateTime}&X-Amz-Expires=${expirationTime}$X-Amz-SignedHeaders=host`;
-
-  // Calculate the payload hash
-  const calculatedPayloadHash = crypto.createHash('sha256').update('').digest('hex');
-
-  // Build the canonical headers and request
-  const canonicalHeaders = `host:${host}\n`;
-  const canonicalRequest = `${method}\n${path}\n${canonicalQuerystring}\n${canonicalHeaders}\nhost\n${calculatedPayloadHash}`;
-
-  // Build the string to sign
-  const stringToSign = `${algorithm}\n${dateTime}\n${credentialScope}\n${crypto.createHash('sha256').update(canonicalRequest).digest('hex')}`;
-
-  // Generate the signing key and compute the signature
-  const signingKey = getSignatureKey(secretAccessKey, date, region, service);
-  const signature = crypto.createHmac('sha256', signingKey).update(stringToSign, 'utf-8').digest('hex');
-
-  // Build the final presigned URL
-  const canonicalQuerystringWithSignature = canonicalQuerystring + '&X-Amz-Signature=' + signature;
-  const canonicalQuerystringWithToken = canonicalQuerystringWithSignature + '&X-Amz-Security-Token=' + encodeURIComponent(sessionToken);
-  const presignedUrl = `${protocol}://${host}${path}?${canonicalQuerystringWithToken}`;
-
-  return presignedUrl;
+  return formatUrl(signedRequest);
 };
 
 export const RealTimeEventsHandlerFactory = () => {
@@ -93,7 +67,7 @@ export const RealTimeEventsHandlerFactory = () => {
     const iotConnectorSecretAccessKey = response.Credentials?.SecretAccessKey ?? '';
     const iotConnectorSessionToken = response.Credentials?.SessionToken ?? '';
 
-    const url = createPresignedURL({
+    const url = await createPresignedURL({
       accessKeyId: iotConnectorAccessKeyId,
       secretAccessKey: iotConnectorSecretAccessKey,
       sessionToken: iotConnectorSessionToken,
@@ -108,6 +82,10 @@ export const RealTimeEventsHandlerFactory = () => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify({
         success: true,
         message: 'Pre-signed url generated successfully',
