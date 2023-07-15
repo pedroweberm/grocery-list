ee63b0c023b27f04520e3534bbe5dda78c4d102a
Pedro Weber Mendonça <pedro.weber@banqi.com>
2023-07-15 02:15:03 -0300

    🐛 Fixed pipelines

--- backend/resolvers/get-iot-endpoint.js
+++ backend/resolvers/get-iot-endpoint.js
@@ -3,7 +3,13 @@ const { IoTClient, DescribeEndpointCommand } = require('@aws-sdk/client-iot')
 module.exports = async ({ resolveVariable }) => {
   const region = await resolveVariable('self:provider.region')
 
-  const response = await new IoTClient({ region }).send(new DescribeEndpointCommand({ endpointType: 'iot:Data-ATS' }))
+  const response = await new IoTClient({
+    region,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    },
+  }).send(new DescribeEndpointCommand({ endpointType: 'iot:Data-ATS' }))
 
   return { endpoint: response.endpointAddress }
 };
