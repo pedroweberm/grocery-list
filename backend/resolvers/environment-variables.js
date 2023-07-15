9520d7d13ca7d2e1d4563debc9974b371e1332d5
Pedro Weber Mendonça <pedro.weber@banqi.com>
2023-07-15 01:03:57 -0300

    🐛 Fixed environment variable resolution

--- backend/resolvers/environment-variables.js
+++ backend/resolvers/environment-variables.js
@@ -1,25 +1,33 @@
 const envVariables = require('../.env.example.json');
 
+const getEnvValuesFromFile = async runningStage => {
+  const variables = require(`../.env.${runningStage}.json`);
+
+  return variables;
+};
+
+const validateVariables = loadedVariables => {
+  const exampleEnvVariables = Object.keys(envVariables);
+
+  exampleEnvVariables.forEach(variableName => {
+    const value = loadedVariables[variableName];
+
+    if (!value || value.length === 0) {
+      throw new Error(`Variable ${variableName} is missing (defined in .env.example.json)`);
+    }
+  });
+};
+
+const getStageName = (rawName) => {
+  if (rawName.includes('pr-')) return 'pr'
+
+  return rawName
+}
+
 module.exports = async ({ resolveVariable }) => {
-  const getEnvValuesFromFile = async runningStage => {
-    const variables = require(`../.env.${runningStage}.json`);
+  const rawStage = await resolveVariable('self:provider.stage');
 
-    return variables;
-  };
-
-  const validateVariables = loadedVariables => {
-    const exampleEnvVariables = Object.keys(envVariables);
-
-    exampleEnvVariables.forEach(variableName => {
-      const value = loadedVariables[variableName];
-
-      if (!value || value.length === 0) {
-        throw new Error(`Variable ${variableName} is missing (defined in .env.example.json)`);
-      }
-    });
-  };
-
-  const stage = await resolveVariable('self:provider.stage');
+  const stage = getStageName(rawStage)
 
   console.log(`Resolving environment variables for '${stage}' stage...`);
 
@@ -48,10 +56,57 @@ module.exports = async ({ resolveVariable }) => {
 
       return variables;
     }
-    case 'testing':
-      return envVariables;
-    case 'production':
-      return envVariables;
+    case 'pr': {
+      const variablesFromFile = await getEnvValuesFromFile(stage);
+      const customVariables = {
+        DYNAMODB_TABLE_NAME: { Ref: 'GroceryListTable' },
+        COGNITO_USER_POOL_ID: { Ref: 'GroceryListCognitoUserPool' },
+        IOT_CONNECTOR_ROLE_ARN: { 'Fn::GetAtt': ['IotConnectorRole', 'Arn'] },
+        IOT_DATA_ENDPOINT: await resolveVariable('file(./resolvers/get-iot-endpoint.js):endpoint'),
+      };
+      const variables = {
+        ...variablesFromFile,
+        ...customVariables,
+      };
+
+      validateVariables(variables);
+
+      return variables;
+    }
+    case 'testing': {
+      const variablesFromFile = await getEnvValuesFromFile(stage);
+      const customVariables = {
+        DYNAMODB_TABLE_NAME: { Ref: 'GroceryListTable' },
+        COGNITO_USER_POOL_ID: { Ref: 'GroceryListCognitoUserPool' },
+        IOT_CONNECTOR_ROLE_ARN: { 'Fn::GetAtt': ['IotConnectorRole', 'Arn'] },
+        IOT_DATA_ENDPOINT: await resolveVariable('file(./resolvers/get-iot-endpoint.js):endpoint'),
+      };
+      const variables = {
+        ...variablesFromFile,
+        ...customVariables,
+      };
+
+      validateVariables(variables);
+
+      return variables;
+    }
+    case 'production': {
+      const variablesFromFile = await getEnvValuesFromFile(stage);
+      const customVariables = {
+        DYNAMODB_TABLE_NAME: { Ref: 'GroceryListTable' },
+        COGNITO_USER_POOL_ID: { Ref: 'GroceryListCognitoUserPool' },
+        IOT_CONNECTOR_ROLE_ARN: { 'Fn::GetAtt': ['IotConnectorRole', 'Arn'] },
+        IOT_DATA_ENDPOINT: await resolveVariable('file(./resolvers/get-iot-endpoint.js):endpoint'),
+      };
+      const variables = {
+        ...variablesFromFile,
+        ...customVariables,
+      };
+
+      validateVariables(variables);
+
+      return variables;
+    }
     default:
       return envVariables;
   }
