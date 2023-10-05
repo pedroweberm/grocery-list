5c32d8c86f7862bfa56f6f9d301466afa09aa227
Pedro Weber Mendonça <pedro.weber@banqi.com>
2023-10-05 12:14:15 -0300

    🚧 Implementing list creation

--- frontend/src/components/rounded-button/rounded-button.tsx
+++ frontend/src/components/rounded-button/rounded-button.tsx
@@ -1,75 +1,117 @@
-import type { PropsWithChildren } from 'react'
-import styled from 'styled-components'
-import { FiArrowRight } from 'react-icons/fi'
+import type { PropsWithChildren } from "react";
+import styled from "styled-components";
+import { FiArrowRight } from "react-icons/fi";
 
-import { colors } from '../../helpers'
-import { SpinningLoader } from '../spinning-loader/spinning-loader'
+import { colors } from "../../helpers";
+import { SpinningLoader } from "../spinning-loader/spinning-loader";
 
 interface RoundeButtonExtraProps {
-  enabled: boolean
-  loading?: boolean
-  icon?: boolean
+  enabled: boolean;
+  loading?: boolean;
+  icon?: boolean;
 
-  onClick: () => unknown
+  onClick: () => unknown;
 
-  text?: string
-  marginLeft?: boolean
-  marginRight?: boolean
-  primary?: boolean
-  secondary?: boolean
+  text?: string;
+  marginLeft?: boolean;
+  marginRight?: boolean;
+  primary?: boolean;
+  secondary?: boolean;
+  className?: string;
 }
 
-const ButtonText = styled.h2<{ primary: boolean, margin?: boolean }>`
+const ButtonText = styled.h2<{ primary: boolean; margin?: boolean }>`
   font-family: poppins;
   font-size: 18px;
   font-weight: 500;
-  color: ${({ primary }) => primary ? colors.purple.lighest : colors.purple.darkest};
+  color: ${({ primary }) =>
+    primary ? colors.purple.lighest : colors.purple.darkest};
+  flex: 1;
 
-  margin-right: ${({ margin }) => margin ? '10px' : '0'};
-`
+  margin-right: ${({ margin }) => (margin ? "10px" : "0")};
+`;
 
-const Button = styled.button<{ primary: boolean, enabled: boolean, marginLeft?: boolean, marginRight?: boolean }>`
-  padding: 0px 20px;
-  height: 80px;
-  border-radius: 20px;
+interface ButtonProps {
+  enabled: boolean;
+  primary: boolean;
+}
 
-  opacity: ${({ enabled }: { enabled: boolean }) => enabled ? '100%' : '40%'};
-
-  background: ${({ primary }) => primary ? colors.purple.darkest : 'transparent'};
-  border-color: ${({ primary }) => primary ? colors.purple.darkest : colors.purple.darkest};
-  border-width: ${({ primary }) => primary ? '2px' : '2px'};
-
-  margin-right: ${({ marginLeft }) => marginLeft ? '10px' : '0px'};
-  margin-left: ${({ marginRight }) => marginRight ? '10px' : '0px'};
-
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+const Button = styled.button.attrs({
+  className: `
+    flex
+    flex-1
+    flex-row
+    items-center
+    content-between
+    border-purple-darkest
+    border-2
+    ${({ className }: { className: string }) => className}
+  `,
+})<{
+  primary: boolean;
+  enabled: boolean;
+  marginLeft?: boolean;
+  marginRight?: boolean;
+}>`
+  border-radius: 10%;
+  opcity: ${({ enabled }: ButtonProps) => (enabled ? "100%" : "40%")};
+  background: ${({ primary }) =>
+    primary ? colors.purple.darkest : "transparent"};
 
   transition: background box-shadow 0.2s;
-  
-  pointer-events: ${({ enabled }: { enabled: boolean }) => enabled ? 'initial' : 'none'};
-  
-  cursor: ${({ enabled }: { enabled: boolean }) => enabled ? 'pointer' : 'not-allowed'};
-  
-  &:hover {
-    background: ${({ enabled, primary }) => enabled ? (primary ? colors.purple.darkest : colors.purple.light) : (colors.grey.light)};
-    box-shadow: 0px 15px 30px ${colors.purple.medium};
-    border-color: ${({ primary }) => primary ? colors.purple.light : colors.purple.darkest};
-  }
-`
 
-export const RoundedButton = ({ loading, text, icon, primary, secondary, onClick, enabled, marginLeft, marginRight }: PropsWithChildren<RoundeButtonExtraProps>) => {
-  const isPrimary = primary === true ? true : secondary === true ? false : true
+  pointer-events: ${({ enabled }: { enabled: boolean }) =>
+    enabled ? "initial" : "none"};
+
+  cursor: ${({ enabled }: { enabled: boolean }) =>
+    enabled ? "pointer" : "not-allowed"};
+
+  &:hover {
+    background: ${({ enabled, primary }) =>
+      enabled
+        ? primary
+          ? colors.purple.darkest
+          : colors.purple.light
+        : colors.grey.light};
+    box-shadow: 0px 15px 30px ${colors.purple.medium};
+    border-color: ${({ primary }) =>
+      primary ? colors.purple.light : colors.purple.darkest};
+  }
+`;
+
+export const RoundedButton = ({
+  loading,
+  text,
+  icon,
+  primary,
+  secondary,
+  onClick,
+  enabled,
+  marginLeft,
+  marginRight,
+  className,
+}: PropsWithChildren<RoundeButtonExtraProps>) => {
+  const isPrimary = primary === true ? true : secondary === true ? false : true;
 
   return (
-    <Button onClick={onClick} primary={isPrimary} enabled={enabled} marginLeft={marginLeft} marginRight={marginRight} >
-      <ButtonText primary={isPrimary} margin={icon}>{text}</ButtonText>
-      {!loading
-        ? icon && <FiArrowRight size={26} color={colors.purple.lighest} />
-        : icon && <SpinningLoader primary={isPrimary} />
-      }
+    <Button
+      onClick={onClick}
+      primary={isPrimary}
+      enabled={enabled}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      className={className}
+    >
+      <ButtonText primary={isPrimary} margin={icon}>
+        {text}
+      </ButtonText>
+      {icon ? (
+        loading ? (
+          <SpinningLoader primary={isPrimary} />
+        ) : (
+          <FiArrowRight size={26} color={colors.purple.lighest} />
+        )
+      ) : undefined}
     </Button>
-  )
-}
+  );
+};
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useCallback, useEffect, useState, PropsWithChildren } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Modal, SmallButton } from "../../components"
-import { useSession } from "../../hooks"
-import { getLists, List } from "../../services/lists/lists"
+import { Modal, RoundedButton, SmallButton, TextInput } from "../../components";
+import { useFormField, useSession } from "../../hooks";
+import { getLists, List, createList } from "../../services/lists/lists";
 
 import {
   MainContainer,
@@ -13,39 +13,87 @@ import {
   ListCard,
   TitleContainer,
   ListTitleContainer,
-  ListTitle
-} from './styles'
+  ListTitle,
+} from "./styles";
+
+const CreateListComponent = ({
+  handleCreateListConfirm,
+  isLoading,
+}: PropsWithChildren<{
+  handleCreateListConfirm: () => unknown;
+  isLoading: boolean;
+}>) => {
+  const {
+    value: newListName,
+    isValid: isNewListNameValid,
+    handleChange: handleNewListNameChange,
+  } = useFormField();
+
+  return (
+    <div className="grid grid-cols-3 grid-rows-5 w-full max-h-56 bg-purple-darkest">
+      <div className="flex flex-col row-span-3 col-span-3">
+        Name:
+        <TextInput
+          handleChange={handleNewListNameChange}
+          isValid={isNewListNameValid}
+          placeholder="My list"
+          type="submit"
+          value={newListName}
+        />
+      </div>
+      <div className="row-span-1 col-span-3" />
+      <div className="row-span-1 col-span-2" />
+      <div className="flex row-span-1 col-span-1">
+        <RoundedButton
+          onClick={handleCreateListConfirm}
+          text="Criar"
+          loading={isLoading}
+          enabled={!isLoading}
+          className="w-3/4"
+          icon={false}
+        />
+      </div>
+    </div>
+  );
+};
 
 export const Home = () => {
-  const [userLists, setUserLists] = useState<List[]>()
-  const [isCreateListModalOpened, setIsCreateListModalOpened] = useState(false)
+  const [userLists, setUserLists] = useState<List[]>();
+  const [isCreateListModalOpened, setIsCreateListModalOpened] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const onInvalid = useCallback(() => navigate('/sign-in'), [navigate])
+  const onInvalid = useCallback(() => navigate("/sign-in"), [navigate]);
 
-  const { token } = useSession({ onInvalid })
+  const { token } = useSession({ onInvalid });
 
   useEffect(() => {
     async function getUserLists() {
       if (token) {
-        const lists = await getLists(token)
+        const lists = await getLists(token);
 
-        setUserLists(lists.data)
+        setUserLists(lists.data);
       }
     }
 
-    getUserLists()
-    console.log(token)
-  }, [token])
+    getUserLists();
+    console.log(token);
+  }, [token]);
 
   const handleListClick = (listId: string) => {
-    navigate(`/lists/${listId}`, { state: { listId } })
-  }
+    navigate(`/lists/${listId}`, { state: { listId } });
+  };
 
   const handleCreateListClick = () => {
-    setIsCreateListModalOpened(true)
-  }
+    setIsCreateListModalOpened(true);
+  };
+
+  const handleCreateListConfirm = useCallback(
+    async (name: string) => {
+      await createList({ name }, token);
+    },
+    [token]
+  );
 
   return (
     <MainContainer>
@@ -63,9 +111,7 @@ export const Home = () => {
           {userLists?.map((list) => (
             <ListCard key={list.id} onClick={() => handleListClick(list.id)}>
               <ListTitleContainer>
-                <ListTitle>
-                  {list.name}
-                </ListTitle>
+                <ListTitle>{list.name}</ListTitle>
               </ListTitleContainer>
             </ListCard>
           ))}
@@ -75,8 +121,10 @@ export const Home = () => {
         open={isCreateListModalOpened}
         setOpen={setIsCreateListModalOpened}
       >
-        Create new list
+        <CreateListComponent
+          handleCreateListConfirm={handleCreateListConfirm}
+        />
       </Modal>
     </MainContainer>
-  )
-}
+  );
+};
